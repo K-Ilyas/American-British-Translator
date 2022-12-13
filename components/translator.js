4a5cb6da4a90bb88f9378a7c1d84c9c65812b71a
K-ilyas <ilyaskritet@gmail.com>
2022-12-13 18:24:07 +0100

    feat : fix span issue

--- components/translator.js
+++ components/translator.js
@@ -9,40 +9,41 @@ class Translator {
         if (locale === "american-to-british") {
             for (let value in americanOnly) {
                 if ((new RegExp("\s?" + value + " ")).test(text.toLowerCase()) || (new RegExp(" " + value + "\\.")).test(text.toLowerCase())) {
-                    translation = translation.replace((new RegExp(value, "i")), "<span class='highlight'>" + americanOnly[value] + "</span>");
+
+                    translation = translation.replace((new RegExp(value, "i")), '<span class="highlight">' + americanOnly[value] + "</span>");
                 }
             }
             for (let value in americanToBritishSpelling) {
                 if ((new RegExp("\s?" + value + " ")).test(text.toLowerCase()) || (new RegExp("\s?" + value + "\\.")).test(text.toLowerCase())) {
-                    translation = translation.replace((new RegExp(value, "i")), "<span class='highlight'>" + americanToBritishSpelling[value] + "</span>");
+                    translation = translation.replace((new RegExp(value, "i")), '<span class="highlight">' + americanToBritishSpelling[value] + "</span>");
                 }
             }
             for (let value in americanToBritishTitles) {
                 if ((new RegExp("\s?" + value.replace(/(\.)/, "\\$1") + " ")).test(text.toLowerCase())) {
-                    translation = translation.replace((new RegExp(value, "i")), "<span class='highlight'>" + americanToBritishTitles[value].charAt(0).toUpperCase() + americanToBritishTitles[value].slice(1) + "</span>");
+                    translation = translation.replace((new RegExp(value, "i")), '<span class="highlight">' + americanToBritishTitles[value].charAt(0).toUpperCase() + americanToBritishTitles[value].slice(1) + "</span>");
                 }
             }
             // time 
-            translation = translation.replace(/(\d{1,2})(:)(\d{1,2})/, "<span class='highlight'>" + "$1.$3" + "</span>");
+            translation = translation.replace(/(\d{1,2})(:)(\d{1,2})/, '<span class="highlight">' + "$1.$3" + "</span>");
         }
         else if (locale === "british-to-american") {
             for (let value in britishOnly) {
                 if ((new RegExp("\s?" + value + " ")).test(text.toLowerCase()) || (new RegExp("\s?" + value + "\\.")).test(text.toLowerCase())) {
-                    translation = translation.replace((new RegExp(value, "i")), "<span class='highlight'>" + britishOnly[value] + "</span>");
+                    translation = translation.replace((new RegExp(value, "i")), '<span class="highlight">' + britishOnly[value] + "</span>");
                 }
             }
             for (let value in americanToBritishSpelling) {
                 if ((new RegExp("\s?" + americanToBritishSpelling[value] + " ")).test(text.toLowerCase()) || (new RegExp("\s?" + americanToBritishSpelling[value] + "\\.")).test(text.toLowerCase())) {
-                    translation = translation.replace((new RegExp(americanToBritishSpelling[value], "i")), "<span class='highlight'>" + value + "</span>");
+                    translation = translation.replace((new RegExp(americanToBritishSpelling[value], "i")), '<span class="highlight">' + value + "</span>");
                 }
             }
             for (let value in americanToBritishTitles) {
                 if ((new RegExp("\s?" + americanToBritishTitles[value].replace(/(\.)/, "\\$1") + " ")).test(text.toLowerCase())) {
-                    translation = translation.replace((new RegExp(americanToBritishTitles[value], "i")), "<span class='highlight'>" + value.charAt(0).toUpperCase() + value.slice(1) + "</span>");
+                    translation = translation.replace((new RegExp(americanToBritishTitles[value], "i")), '<span class="highlight">' + value.charAt(0).toUpperCase() + value.slice(1) + "</span>");
                 }
             }
             // time 
-            translation = translation.replace(/(\d{1,2})(.)(\d{1,2})/, "<span class='highlight'>" + "$1:$3" + "</span>");
+            translation = translation.replace(/(\d{1,2})(.)(\d{1,2})/, '<span class="highlight">' + "$1:$3" + "</span>");
         }
         return /<\/span>/.test(translation) ? translation : "Everything looks good to me!";
     }
--- controllers/translator-fields.js
+++ controllers/translator-fields.js
@@ -5,8 +5,10 @@ const empty = require("is-empty");
 // control required fields
 exports.requiredFields = function (req, res, next) {
     const { text, locale } = req.body;
-    if (!text || !locale)
+    if (!("text" in req.body) || !("locale" in req.body)) {
+
         res.status(200).json({ error: 'Required field(s) missing' });
+    }
     else if (empty(text))
         res.status(200).json({ error: 'No text to translate' });
     else if (locale !== "american-to-british" && locale !== "british-to-american")
